7d9e27acbbda7eab7e25a3330dd45e8a309e9a10
Chris Patuzzo <chris@patuzzo.co.uk>
2014-08-09 07:37:27 +0000

    Set up user and lock down ssh

---
+++ cookbooks/users/recipes/default.rb
@@ -0,0 +1,39 @@
+user "chris" do
+  supports manage_home: true
+  home "/home/chris"
+end
+
+execute "unlock user" do
+  command "usermod -p '*' chris"
+end
+
+group "sudo" do
+  action :modify
+  append true
+  members "chris"
+end
+
+cookbook_file "/etc/sudoers" do
+  mode "0440"
+end
+
+directory "/home/chris/.ssh" do
+  mode "0700"
+  owner "chris"
+  group "chris"
+end
+
+cookbook_file "/home/chris/.ssh/authorized_keys" do
+  source "id_rsa.pub"
+  mode "0644"
+  owner "chris"
+  group "chris"
+end
+
+cookbook_file "/etc/ssh/sshd_config" do
+  mode "0644"
+end
+
+service "ssh" do
+  action :restart
+end
---
+++ solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/etc/chef"
+cookbook_path "/etc/chef/cookbooks"
+json_attribs "/etc/chef/node.json"
