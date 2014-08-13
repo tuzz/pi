d00e0e038e69b9fdf0a9b12dac102fc80cf65450
Chris Patuzzo <chris@patuzzo.co.uk>
2014-08-13 20:15:22 +0000

    Disable chef-client runs

--- cookbooks/clean/recipes/default.rb
+++ cookbooks/clean/recipes/default.rb
@@ -14,3 +14,17 @@ execute "clean up home directories" do
 
   only_if { File.exists?("/root/.bashrc") }
 end
+
+execute "prevent chef-client runs" do
+  command <<-CMD
+    rm /etc/init.d/chef-client
+    update-rc.d chef-client remove
+  CMD
+  notifies :run, "execute[reboot]", :delayed
+
+  only_if { File.exists?("/etc/init.d/chef-client") }
+end
+
+execute "reboot" do
+  action :nothing
+end
